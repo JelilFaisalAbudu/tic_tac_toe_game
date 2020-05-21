fe42fcb2185ff3267654d3d1950d15eefda400fb
Albertino Francisco <albertino@ua.pt>
2020-05-21 11:42:11 +0100

    Uupdated user interface inputs and outputs.

--- bin/main.rb
+++ bin/main.rb
@@ -1,20 +1,27 @@
-
 #!/usr/bin/env ruby
-display_instruction() # This method wil welcome users and ask whether to display instructions or not.
-input_symb = gets.chomp.upcase! # Read player option from the user.
-if  input_symb == "O"
-  player_one = User.new("O")
-  player_two = User.new("X")
-elsif input_symb == "X"
-  player_one = User.new("X")
-  player_two = User.new("O")
-else
-  puts "Please choose between X and O"
+# display_instruction(): This method wil welcome users and ask whether to display instructions or not.
+puts 'Wellcome to the tic-tac-toe geme!'
+
+input_symb = ''
+until %w[X O].include?(input_symb)
+  puts 'Please choose between X and O'
+  input_symb = gets.chomp.upcase! # Read player option from the user.
 end
-play_record = {} # Record the players' input in the hash to represent an array.
-current_board = Board.new(player_one, player_two)
-display_board(current_board)
-play(player_one, player_two, current_board)
-check_results(player_one, player_two, play_record)
-display_results()
-winner()
+puts "You have chosen #{input_symb} as your game symbol."
+if input_symb == 'O'
+  #   player_one = User.new("O")
+  #   player_two = User.new("X")
+  puts 'Your opponent will play with the game symbol X.'
+else
+  #   player_one = User.new("X")
+  #   player_two = User.new("O")
+  puts 'Your opponent will play with the game symbol O.'
+end
+
+# play_record = {} # Record the players' input in the hash to represent an array.
+# current_board = Board.new(player_one, player_two)
+# display_board(current_board)
+# play(player_one, player_two, current_board)
+# check_results(player_one, player_two, play_record)
+# display_results()
+# winner()
