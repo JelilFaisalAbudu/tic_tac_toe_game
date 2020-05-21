d157ced1735d49518e127f4c2ba0c0c0fec88584
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-05-21 09:53:57 +0000

    Added comments to guide follow the coding with ease.

--- bin/main.rb
+++ bin/main.rb
@@ -1,21 +1,20 @@
 
 #!/usr/bin/env ruby
-puts "Hello World!"
-wellcome_message("O", "X")
-input_symb = gets.chomp
-if input_symb = "o" || input_symb = "O"
+display_instruction() # This method wil welcome users and ask whether to display instructions or not.
+input_symb = gets.chomp.upcase! # Read player option from the user.
+if  input_symb == "O"
   player_one = User.new("O")
   player_two = User.new("X")
-elsif input_symb = "x" || input_symb = "X"
+elsif input_symb == "X"
   player_one = User.new("X")
   player_two = User.new("O")
 else
   puts "Please choose between X and O"
 end
-play_record = []
+play_record = {} # Record the players' input in the hash to represent an array.
 current_board = Board.new(player_one, player_two)
 display_board(current_board)
 play(player_one, player_two, current_board)
-check_results(player_one, player_tow, play_record)
+check_results(player_one, player_two, play_record)
 display_results()
 winner()
