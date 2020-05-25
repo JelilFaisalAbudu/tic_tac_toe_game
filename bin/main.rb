7c65012a28f834255a3125bbb76254a01a4fc9b5
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-05-25 09:41:53 +0000

    Add a welcome message to the game add ask the user to choose between the given leeters they would like to use

--- bin/main.rb
+++ bin/main.rb
@@ -11,4 +11,22 @@ def display_board(board_cells)
   puts ''
 end
 
+puts 'Wellcome to the tic-tac-toe geme!'
+
+board = ['', 1, 2, 3, 4, 5, 6, 7, 8, 9]
+display_board(board)
+input_symb1 = ''
+until %w[X O].include?(input_symb1)
+  puts 'Please choose between X and O to play with.'
+  input_symb1 = gets.chomp.upcase! # Read player option from the user.
+end
+puts "You have chosen #{input_symb1} as your game symbol."
+input_symb2 = if input_symb1 == 'O'
+                'X'
+              else
+                'O'
+              end
+
+puts "Your opponent will play with the game #{input_symb2}"
+
 # rubocop :enable
