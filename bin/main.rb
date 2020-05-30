8f5be48f951eb29db535642b2f19700f4e1f3552
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-05-30 14:44:33 +0000

    Modify the user interface

--- bin/main.rb
+++ bin/main.rb
@@ -4,7 +4,7 @@ require_relative '../lib/game_board.rb'
 require_relative '../lib/player.rb'
 require_relative '../lib/game.rb'
 
-puts '----------Welcome to Tic-Tac-Toe game----------'
+puts '--------Welcome to Tic-Tac-Toe game--------'
 puts 'Please choose a name and favourite letters to play with.'
 
 puts 'First player\'s name'
@@ -30,11 +30,34 @@ players = { player_one.player_name => player_one.player_marker, player_two.playe
 game = Game.new(players)
 play_again = true
 while play_again
-  game.play
-  puts 'Would you like to play again? (yes/y or no/n)'
-  play_again = gets.chomp.downcase
-  play_again = %w[yes y].include?(play_again) || false
+  game.shuffle_players
+  puts game.message_instance.first_player(game.current_player)
+  loop do
+    puts game.message_instance.give_msg(game.display_game_board)
+    puts game.message_instance.move_msg_to(game.current_player)
+    pos = gets.chomp.to_i
+    until game.board_instance.valid_move?(pos)
+      puts game.display_game_board
+      puts game.message_instance.invalid_move_msg
+      puts "#{game.current_player}, make a valid move."
+      pos = gets.chomp.to_i
+    end
+    game.board_instance.update_board(game.players[game.current_player], pos)
+    if game.winner?(game.players[game.current_player])
+      puts game.message_instance.give_msg(game.display_game_board)
+      puts game.message_instance.win_msg(game.current_player)
+      break
+    elsif game.draw?
+      puts game.message_instance.give_msg(game.display_game_board)
+      puts game.message_instance.draw_msg
+      break
+    else
+      game.switch_players
+    end
+  end
   game.reset_game_board
+  puts 'Would you like to play again? (yes(y) or no(n))'
+  play_again = gets.chomp.downcase
+  play_again = %w[yes y].include?(play_again) ? true : false
 end
-
 puts 'Goodbye guys! See you next time.'
