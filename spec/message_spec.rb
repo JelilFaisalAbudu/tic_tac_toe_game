392019842930dc9b09bf0431299f98f9d3e2d115
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-06-11 13:20:53 +0000

    Add test codes for Message#give_msg and Message#first_player

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,1 +1,19 @@
-purs 'Hello Ruby'
+require_relative '../lib/message.rb'
+
+describe Message do
+  let(:message) { Message.new }
+
+  context '#give_msg' do
+    it 'returns the string version of the given parameter' do
+      expect(message.give_msg(45_215)).to eql('45215')
+    end
+  end
+
+  context '#first_player' do
+    let(:msg) { 'Jelil has randomly been selected as the first player.' }
+
+    it 'returns a hard coded message with the value of the argument given' do
+      expect(message.first_player('Jelil')).to eql('msg')
+    end
+  end
+end
