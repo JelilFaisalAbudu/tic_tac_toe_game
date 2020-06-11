9353aaed1872f0226ab86a3b8b90a362e5671ebe
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-06-11 13:39:35 +0000

    Add test codes for Message#win_msg and Message#draw_msg

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -22,4 +22,21 @@ describe Message do
       expect(message.move_msg_to('Jelil')).to eql('Jelil, make your move')
     end
   end
+  context '#invalid_move_msg' do
+    it 'returns a hard coded message from the instance method' do
+      expect(message.invalid_move_msg).to eql('That\'s an invalid move.')
+    end
+  end
+
+  context '#win_msg' do
+    it 'returns a congratulation message to the player' do
+      expect(message.win_msg('Jelil')).to eql('Congrats! Jelil. You\'ve won.')
+    end
+  end
+
+  context '#draw_msg' do
+    it 'returns a draw message to the players' do
+      expect(message.draw_msg).to eql('It\'s a DRAW.')
+    end
+  end
 end
